e8b929ca8ddff9743b305b7cb54574388aafa38e
kojix2 <2xijok@gmail.com>
2025-07-29 15:07:18 +0900

    First commit

---
+++ lib/igraph.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "igraph/version"
+
+module Igraph
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/igraph/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Igraph
+  VERSION = "0.1.0"
+end
